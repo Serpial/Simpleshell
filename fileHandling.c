eb2e3be37f85b05bec2183ce9314c5358ddb651e
Mohammed Adil Ali Akbar CS2017 <mohammed.akbar.2017@uni.strath.ac.uk>
2019-02-12 11:40:00 +0000

    Update fileHandling.c

--- fileHandling.c
+++ fileHandling.c
@@ -1,31 +1,24 @@
-void fileRead(){
-   FILE *fopen(), *fp;
-   int x;
-   fp = fopen("history.txt","r");
-   x = getc(fp);
-   while (x!= EOF){
-   		putchar(x);
-		x = getc(fp);
-		printf(%s,x);
-   }
-   fclose(fp);
-}
+const int MAX_HISTORY_SIZE = 20;
 
-void fileWrite(char *c){
-	FILE *fp;
-	file = fopen("history.txt","w");
-	fprintf(fp,"%s",c);
-	fclose(fp);
+char * histArray[MAX_HISTORY_SIZE];
+for (int i = 0; i <= MAX_HISTORY_SIZE; i++) {
+    histArray[i] = (char * ) malloc(512);
+    strcpy(histArray[i], "\0");
 }
+int rear = 0;
 
-void fileAppend(char *c){
+	
+void writeHistory(char *c, char * *histArray, int *rear){
+	strcpy(histArray[* rear], c);
+	* rear = (* rear+1)%MAX_HISTORY_SIZE
+	
 	FILE *fp;
 	file = fopen("history.txt","a");
 	fprintf(fp,"%s",c);
 	fclose(fp);
 }
 
-void fileAppendOrRead(char *c){
+void readHistory(char * *histArray, int *rear){
 	FILE *fp;
 	int x;
 	file = fopen("history.txt","a+");
@@ -35,10 +28,24 @@ void fileAppendOrRead(char *c){
 	while (x!= EOF){
 		putchar(x);
 		x = getc(fp);
-		printf(%s,x);
+		strcpy(histArray[* rear], x);
+		* rear = (* rear+1)%MAX_HISTORY_SIZE
 	}
 	
-	//Write
-	fprintf(fp,"%s",c);
+	
+	fclose(fp);
+}
+
+void printHistory(){
+	FILE *fp;
+	int x;
+	file = fopen("history.txt","a+");
+	
+	x = getc(fp);
+	while (x!= EOF){
+		putchar(x);
+		x = getc(fp);
+		printf(%s,x);
+	}
 	fclose(fp);
 }
