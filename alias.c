f666608fdf7cfa7861616d4225b555791f152152
Paul Hutchison <paul.hutchison.2017@uni.strath.ac.uk>
2019-03-17 03:16:06 +0000

    fixed an issues with the program adding a null character as an alias

--- alias.c
+++ alias.c
@@ -33,62 +33,62 @@ void printAlias(char *alias[MAX_ALIAS_SIZE][2]){
 }
 
 /*
-addAlias: allows the user to create a new alias for a command.
+  addAlias: allows the user to create a new alias for a command.
 */
 
 void addAlias(char**phrase, char *alias[MAX_ALIAS_SIZE][2]){
-if (phrase[1] == NULL){
-    printf("Not enough arguments\n");
-    return;
-}
-else if(phrase[2] == NULL){
-printf("Not enough arguments\n");
-    return;
-}
-
-
-//char name[] = " ";
-char command [512] = "";
-int index = 2;
-int j;
-int found = 0;
-//find how mnany arguments there are 
-while(phrase[index] != NULL){
- index++;
-}
-
-//makes command for alias
-for (j =2; j<index; j++){
-    strcat(command, phrase[j]);
-    strcat(command, " ");
-}
-
-
-
-//counts null charatcers
-int nullEntries=0;
-nullEntries = howManyNullSpaces(alias);
-
- //checks list of previous alias 
- for (j =0; j<(MAX_ALIAS_SIZE-nullEntries); j++){
-    if (strcmp(phrase[1], alias[j][0]) == 0){
-        printf("overwriting previous alias\n");
-        alias[j][1] = strdup(command);
-        found = 1;
+    if (phrase[1] == NULL){
+        printf("Not enough arguments\n");
+        return;
     }
- }
-
- //checks if alias is full if not adds
- if(nullEntries == 0){
-    printf("no more space for alias. please unalias or overwrite a previous alias\n");
-    //return;
+    else if(phrase[2] == NULL){
+        printf("Not enough arguments\n");
+        return;
     }
- else if (nullEntries != 0 && found == 0){
-    alias[MAX_ALIAS_SIZE-nullEntries][0] =strdup(phrase[1]);
-    alias[MAX_ALIAS_SIZE-nullEntries][1] =strdup(command);
+
+
+    //char name[] = " ";
+    char command [512] = "";
+    int index = 2;
+    int j;
+    int found = 0;
+    //find how mnany arguments there are 
+    while(phrase[index] != NULL){
+        index++;
+    }
+
+    //makes command for alias
+    for (j =2; j<index; j++){
+        strcat(command, phrase[j]);
+        strcat(command, " ");
+    }
+
+
+
+    //counts null charatcers
+    int nullEntries=0;
+    nullEntries = howManyNullSpaces(alias);
+
+    //checks list of previous alias 
+    for (j =0; j<(MAX_ALIAS_SIZE-nullEntries); j++){
+        if (strcmp(phrase[1], alias[j][0]) == 0){
+            printf("overwriting previous alias\n");
+            alias[j][1] = strdup(command);
+            found = 1;
+        }
+    }
+
+    //checks if alias is full if not adds
+    if(nullEntries == 0){
+        printf("no more space for alias. please unalias or overwrite a previous alias\n");
+        //return;
+    }
+    else if (nullEntries != 0 && found == 0){
+        alias[MAX_ALIAS_SIZE-nullEntries][0] =strdup(phrase[1]);
+        alias[MAX_ALIAS_SIZE-nullEntries][1] =strdup(command);
   
 
- }
+    }
 
 
 }
@@ -147,49 +147,28 @@ void invokeAlias(char* instruction, char *alias[MAX_ALIAS_SIZE][2]){
     //printf("alias called:");
     int i;
     int nullEntries;
- char temp[512];
- char temp2[512];
-char *p = temp;
-char*q = temp2;
+    char temp[512];
+    char temp2[512];
+    char *p = temp;
+    char*q = temp2;
     nullEntries = howManyNullSpaces(alias);
     for (i = 0; i<MAX_ALIAS_SIZE-nullEntries; i++){
         if (alias[i][0] != NULL){
             if (strcmp(instruction, alias[i][0]) == 0){
-                    for(int j=0; j<MAX_ALIAS_SIZE-nullEntries; j++){ //this for is stage 9
-                        strcpy(temp, alias[i][1]);
-                        p[strlen(p)-1]=0;
-                         if(strcmp(alias[j][0], temp)==0){
+                for(int j=0; j<MAX_ALIAS_SIZE-nullEntries; j++){ //this for is stage 9
+                    strcpy(temp, alias[i][1]);
+                    p[strlen(p)-1]=0;
+                    if(strcmp(alias[j][0], temp)==0){
 
-                            strcpy(instruction, alias[j][1]);
-                            return;
-                         }
-                         
-
+                        strcpy(instruction, alias[j][1]);
+                        return;
                     }
-
-                strcpy(instruction, alias[i][1]);
-
-
                 }
-
+                strcpy(instruction, alias[i][1]);
             }
-
         }
     }
-
-                     
-                      
-            
-        
-        
-
-          
-    
-
-
-
-
-
+}
 
 /*
 readAliases: Similar to History.c, readAliases reads in a file at a speciified
@@ -200,7 +179,8 @@ void readAliases (char *alias[MAX_ALIAS_SIZE][2]) {
     FILE *fp;
     char fileLocation[MAX_INSTR]="";
     char joinedAlias[MAX_INSTR]="", temp[MAX_INSTR];
-    int numAlias=0, len, letterIndex, pastBar = 0;
+    int numAlias=0, len, letterIndex, pastBar = 0, counter;
+    char *tempAlias[MAX_ALIAS_SIZE][2]={{""}};
     
     strcpy(fileLocation, getenv("HOME"));
     strcat(fileLocation, "/.aliases");
@@ -225,11 +205,20 @@ void readAliases (char *alias[MAX_ALIAS_SIZE][2]) {
                     break;
                 }
             }
-            addAlias(parseInput(joinedAlias), alias);
+            addAlias(parseInput(joinedAlias), tempAlias);
         } else {
             printf("Error: Unable to add alias:%s\n", joinedAlias);
         }
     }
+    counter=0;
+    for (int i=0; tempAlias[i][0]!=NULL; i++) {
+        if (tempAlias[i][0]!=NULL && tempAlias[i][1]!=NULL) {
+            alias[counter][0] = tempAlias[i][0];
+            alias[counter][1] = tempAlias[i][1];
+            counter++;
+        }
+    }
+    if (counter==0) {alias[0][1] = "";}
     fclose(fp);
 }
 
@@ -257,19 +246,14 @@ void writeAliases(char *alias[MAX_ALIAS_SIZE][2]) {
     }
 }
 
-
-
-
-
-
 //utlitly fucntion for alaias 
 int howManyNullSpaces(char *alias[MAX_ALIAS_SIZE][2]){
-int k; 
-int nullEntries=0;
- for (k=0; k<MAX_ALIAS_SIZE; k++){
-    if (alias[k][0] == NULL){
-        nullEntries++; 
+    int k; 
+    int nullEntries=0;
+    for (k=0; k<MAX_ALIAS_SIZE; k++){
+        if (alias[k][0] == NULL){
+            nullEntries++; 
+        }
     }
- }
- return nullEntries;   
+    return nullEntries;   
 }
